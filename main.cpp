50f1a3a60d8616e5f038183ca2224cb6a5810246
WMDantibirth <40955304+WMDantibirth@users.noreply.github.com>
2019-04-15 02:42:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,13 +14,23 @@ map<string,vector<int> >f(istream& a,const vector<string>& b)
 {
     map<string,vector<int> >out;
     vector<int>aa;
-    aa.push_back(1);
-    out["a"]=aa;
+    int i = 0;
+    string line;
+    while(getline(a,line)){
+        istringstream ss(line);
+        string word;
+        while(ss>>word){
+            if(find(b.begin(),b.end(),word)!=b.end())out[word].push_back(i);
+        }
+    }
     return out;
 }
 
 int main(/*int argc, char*argv[]*/)
 {
+    /*ofstream file;
+    file.open("123");
+    file.close();*/
     char keywords[]="keywords";
     char text2search[]="text2search";
     //string result=argc[3];
@@ -29,18 +40,19 @@ int main(/*int argc, char*argv[]*/)
     input_file1.open(keywords);
     input_file2.open(text2search);
     //out.open(result, ios::out);
-    cout<<input_file1<<endl;
     string a;
     vector<string>key;
     while(input_file1>>a){
         key.push_back(a);
     }
     input_file1.close();
-    cout<<key.size();
     map<string,vector<int> >out = f(input_file2,key);
     input_file2.close();
-    for(unsigned int i=0;i<key.size();i++){
-        cout<<key[i]<<endl;
+    for(int i=0;i<key.size();i++)
+    {
+        for(int j=0;j<out{i}.size();j++){
+            cout<<out{i}[j]<<endl;
+        }
     }
     return 0;
 }
