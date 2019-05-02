8a522a016ff670d22d1a77cf6667832afd75397c
WMDantibirth <40955304+WMDantibirth@users.noreply.github.com>
2019-05-02 22:02:18 +0800

    Update integerSet.h

--- Assignment3_A Class Definition/integerSet.h
+++ Assignment3_A Class Definition/integerSet.h
@@ -20,20 +20,21 @@ public:
     int* element();//
     void insert(int); //
     void erase(int); //
-    bool isSubset(integerSet); //
+    bool isSubset(integerSet&); //
     bool isMember(int); //
     void print(); //
     int size(); //
     int capacity(); //
     bool isEmpty(); //
     void clear(); //
-    integerSet setunion(integerSet);//
-    integerSet setintsection(integerSet);//
-    integerSet setdifference(integerSet);//
-    integerSet setsymmetricdifference(integerSet);//
+    integerSet setunion(integerSet&);//
+    integerSet setintsection(integerSet&);//
+    integerSet setdifference(integerSet&);//
+    integerSet setsymmetricdifference(integerSet&);//
+    /*void operator=(integerSet);*/
 
     struct bad_integerSet{
-        int errnum;
+        int errnum=0;
     };
 };
 
