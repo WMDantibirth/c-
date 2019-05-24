796649d64f85d5d2425bb1dba764a3da71b2041e
wow?! <40955304+WMDantibirth@users.noreply.github.com>
2019-05-24 09:43:13 -0700

    Add files via upload

---
+++ Assignment4_ A Class Definition(Revisited)/cmake-build-debug/CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/c++/Assignment4_A Class Definition(Revisted)/integerSet.cpp" "E:/c++/Assignment4_A Class Definition(Revisted)/cmake-build-debug/CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/integerSet.cpp.obj"
+  "E:/c++/Assignment4_A Class Definition(Revisted)/main.cpp" "E:/c++/Assignment4_A Class Definition(Revisted)/cmake-build-debug/CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment4_ A Class Definition(Revisited)/cmake-build-debug/CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/main.cpp.obj"
+  "CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/integerSet.cpp.obj"
+  "Assignment4_A_Class_Definition_Revisted_.pdb"
+  "Assignment4_A_Class_Definition_Revisted_.exe"
+  "Assignment4_A_Class_Definition_Revisted_.exe.manifest"
+  "libAssignment4_A_Class_Definition_Revisted_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment4_A_Class_Definition_Revisted_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
