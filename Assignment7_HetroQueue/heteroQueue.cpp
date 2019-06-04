0534e62c606c4b92a1d5b5321b0bdbefaaa6de96
wow?! <40955304+WMDantibirth@users.noreply.github.com>
2019-06-04 19:59:21 +0800

    Update heteroQueue.cpp

--- Assignment7_HetroQueue/heteroQueue.cpp
+++ Assignment7_HetroQueue/heteroQueue.cpp
@@ -1,11 +1,62 @@
+//
+// Created by wmd on 2019/6/4.
+//
+
 #include "heteroQueue.h"
-
-heteroQueue::heteroQueue()
+#include <iostream>
+using  namespace std;
+Hqueue::Hqueue()
 {
-    //ctor
+    Hq=new Base* [400];
+    num=0;
 }
-
-heteroQueue::~heteroQueue()
+Hqueue::~Hqueue()
 {
-    //dtor
+    delete[]Hq;
+}
+bool Hqueue::isEmpty()
+{
+    return num==0;
+}
+void Hqueue::enqueue(Base *ii)
+{
+    Hq[qian]=ii;
+    qian++;
+    num++;
+}
+void Hqueue::dequeue()
+{
+    (*Hq[hou]).print();
+    hou++;
+    num--;
+}
+CHAR::CHAR(char a)
+{
+    charzi=a;
+}
+void CHAR::print()
+{
+    cout<<"CHAR:   "<<charzi<<endl;
+}
+INT::INT(int a)
+{
+    intzi=a;
+}
+void INT::print()
+{
+    cout<<"INT:   "<<intzi<<endl;
+}
+PhoneNumber::PhoneNumber()
+{
+    num=0;
+    name="";
+}
+void PhoneNumber::print()
+{
+    cout<<"PhoneNumber:  ("<<name<<","<<num<<")"<<endl;
+}
+istream & operator>>(istream & is,PhoneNumber &pn)
+{
+    is>>pn.name>>pn.num;
+    return is;
 }
