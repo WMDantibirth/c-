f3375a791dc55a12a39e6ec0dc5578a44ebfe53b
wow?! <40955304+WMDantibirth@users.noreply.github.com>
2019-06-09 21:20:34 -0700

    Add files via upload

---
+++ Assignment8_framework/framework.h
@@ -0,0 +1,18 @@
+#ifndef FRAMEWORK_H // framework.h
+#define FRAMEWORK_H
+
+struct Filter
+{
+  Filter() { }
+  virtual ~Filter() { }
+
+  virtual void start() { }
+  virtual int read()=0;
+  virtual void write() { }
+  virtual void compute() { }
+  virtual int result()=0;
+};
+
+#include "yourApp.h"
+
+#endif
