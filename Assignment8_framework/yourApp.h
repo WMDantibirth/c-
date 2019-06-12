9b5f21eb97de92457d366a83723717a14eb0f3aa
wow?! <40955304+WMDantibirth@users.noreply.github.com>
2019-06-12 17:31:59 +0800

    Update yourApp.h

--- Assignment8_framework/yourApp.h
+++ Assignment8_framework/yourApp.h
@@ -6,13 +6,15 @@ using namespace std;
 
 class Your_filter: public Filter
 {
-    istream is;ostream os;int nline=0;string line;int nchar;
-    public:
-        Your_filter(istream&,ostream&);
-        int read();
-        void write();
-        void compute();
-        int result();
+private:
+    istream&is;ostream&os;
+    int nline=0;string line;int nchar;
+public:
+    Your_filter(istream&,ostream&);
+    int read();
+    void write();
+    void compute();
+    int result();
 };
 
 #endif // YOURAPP_H
